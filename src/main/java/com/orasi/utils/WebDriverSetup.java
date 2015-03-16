1e04343f07db2823eead1cdea31ce0b27e7b2cd3
waitsavery <wwavery0352@yahoo.com>
2015-03-16 09:41:39 -0400

    test

--- src/main/java/com/orasi/utils/WebDriverSetup.java
+++ src/main/java/com/orasi/utils/WebDriverSetup.java
@@ -271,9 +271,9 @@ public class WebDriverSetup{
 				if(!getBrowserUnderTest().equalsIgnoreCase("html")){
 					caps.setVersion(getBrowserVersion());
 				}
-				
 				caps.setPlatform(org.openqa.selenium.Platform.valueOf(getOperatingSystem()));
 				caps.setCapability("name", getTestName());
+				System.out.println("\n\nVERSION:  " + caps.getVersion() + "\n\n");
 		    	driver = new RemoteWebDriver(new URL(getSeleniumHubURL()), caps);
 			} catch (MalformedURLException e) {
 				throw new RuntimeException("Selenium Hub URL set is not a valid URL: " + seleniumHubURL);
