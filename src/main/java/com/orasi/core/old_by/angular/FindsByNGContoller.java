01298b8f2b61d1a4d2ce9e7ee9a412cdb06cdd0e
Justin Phlegar <Justin.Phlegar@orasi.com>
2015-05-18 16:05:40 -0400

    Revert "removed 'old' from angular and by.angular packages"
    This reverts commit 9e0f7f499e976ebb12d14a4fd348453cb8965afa.

---
+++ src/main/java/com/orasi/core/old_by/angular/FindsByNGContoller.java
@@ -0,0 +1,10 @@
+package com.orasi.core.old_by.angular;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+	public interface FindsByNGContoller {
+	  WebElement findElementByNGContoller(String using);
+	  List<WebElement> findElementsByNGContoller(String using);
+	}
+
