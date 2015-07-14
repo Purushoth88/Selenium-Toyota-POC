f0578dce02f2e21111e3f4cd053b8348c94a7c06
waitsavery <wwavery0352@yahoo.com>
2015-07-14 07:27:53 -0400

    testing change to a new project build XML

--- src/main/java/com/orasi/utils/TestEnvironment.java
+++ src/main/java/com/orasi/utils/TestEnvironment.java
@@ -584,7 +584,7 @@ public class TestEnvironment {
 			updates.put("passed", true);
 		}
 
-		updates.put("build", "Build - 293");
+		updates.put("build", "Build - 295");
 		
 		JSONArray tags = new JSONArray();
 		String[] groups = test.getMethod().getGroups();
