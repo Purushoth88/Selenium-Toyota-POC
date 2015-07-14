cf7f1eca69042d4ee39efe6ca859ae20111e921c
waitsavery <wwavery0352@yahoo.com>
2015-07-14 14:53:57 -0400

    pushing

--- src/main/java/com/orasi/utils/TestEnvironment.java
+++ src/main/java/com/orasi/utils/TestEnvironment.java
@@ -584,7 +584,7 @@ public class TestEnvironment {
 			updates.put("passed", true);
 		}
 
-		updates.put("build", "Build - 298");
+		updates.put("build", "Build - 299");
 		
 		JSONArray tags = new JSONArray();
 		String[] groups = test.getMethod().getGroups();
